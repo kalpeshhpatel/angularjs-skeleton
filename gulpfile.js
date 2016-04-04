ed3ed40fc663562d5b3a60c9cc0fec034e103997
Kalpesh Patel <er.kalpesh.h.patel@gmail.com>
2016-04-04 12:31:47 +0530

    Update gulpfile.js
    Updated gulp task to copy index-dev.html first

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var runSequence = require('run-sequence');
 var Promise = require('es6-promise').Promise;
+var rename = require("gulp-rename");
 
 
 
@@ -21,6 +22,11 @@ var bases = {
 };
 
 
+gulp.task('copyIndexDev',function(){
+    return gulp.src(bases.app + 'index-dev.html')
+        .pipe(rename({ basename: 'index'}))
+        .pipe(gulp.dest(bases.dist));
+});
 
 //process index.html file
 //Combine all script tag to single one and merge it
@@ -38,6 +44,6 @@ gulp.task('indexHtml',function() {
 
 // Define the default task as a sequence of the above tasks
 gulp.task('default', function(callback) {
-    runSequence('indexHtml',callback);
+    runSequence('copyIndexDev','indexHtml',callback);
 });
 
