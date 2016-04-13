0f3a741f63e55f6fcf142d4860edc3c26827ffd7
Kalpesh Patel <er.kalpesh.h.patel@gmail.com>
2016-04-13 12:46:59 +0530

    Update helper.js

--- src/app/utilities/helper.js
+++ src/app/utilities/helper.js
@@ -3,7 +3,19 @@
 
 APP.factory('AppHelpers',function() {
 
-    return {
+    var factory = {};
 
-    }
+    /**
+     * Utility function to reset form
+     * @param form
+     */
+    factory.resetForm = function(form) {
+        console.log(form);
+        if(form) {
+            form.$setPristine();
+            form.$setUntouched();
+        }
+    };
+
+    return factory;
 });
